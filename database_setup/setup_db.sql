ea3ec075bb048c173ebd65c630955d0baf556b0f
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2025-02-03 20:21:38 -0800

    ADD script to create database table

---
+++ database_setup/setup_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    cover VARCHAR(255),
+    price DECIMAL(10, 2) NOT NULL
+);
