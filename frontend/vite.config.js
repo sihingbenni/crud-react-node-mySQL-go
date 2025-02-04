3ba12a891c3b1c0d5ef949bcdbbf63d3e3b9235e
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2025-02-03 20:25:12 -0800

    FIX configure vite server host to 0.0.0.0 so it works on localhost and docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // tell the server to listen to all network interfaces
+  // by binding to 0.0.0.0
+  server: {
+    host: '0.0.0.0'
+  }
 })
